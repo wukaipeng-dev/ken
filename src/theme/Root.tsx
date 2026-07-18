41335838c1c820e8b98fce2948240b8dbfdc99c4
wukaipeng <wkpcoder@163.com>
2026-07-18 22:25:36 +0800

    feat: add vercel speed-insights

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 export default function Root({ children }) {
   return (
     <>
       {children}
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
